2fd7b422ef32be002a4d196d38c84107dc2e4944
jhsAbe <abeshi06@hotmail.co.jp>
2016-10-19 10:47:49 +0900

    タグ登録

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -69,6 +69,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:name, :call, :status, :photo, :whatday, :date, :favolite)
+      params.require(:item).permit(:name, :call, :status, :photo, :whatday, :date, :favolite, :tag_list)
     end
 end
